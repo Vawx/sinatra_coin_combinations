72b7eef26dd953cf6bfa2ae53ee63347aebe6291
Greg Kanteres and Kyle Langley <gregkanteres@gmail.com>
2016-01-07 10:29:21 -0800

    added integrated test

--- spec/coin_combinations_integration_spec.rb
+++ spec/coin_combinations_integration_spec.rb
@@ -2,3 +2,12 @@ require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
+
+describe('the coin change path', {:type => :feature}) do
+  it('processes the user input and returns thier change') do
+    visit('/')
+    fill_in('change', :with => "34")
+    click_button('send')
+    expect(page).to have_content("1 quarter 1 nickel 4 cents")
+  end
+end
