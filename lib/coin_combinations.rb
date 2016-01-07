6fb5a08fad865a9f6df161d09761ab538bee425c
Greg Kanteres and Kyle Langley <gregkanteres@gmail.com>
2016-01-07 08:37:32 -0800

    includes spec for single penny

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -1,15 +1,31 @@
 class Fixnum
-  coin_types_singlular = { 1 => "penny",
-                           5 => "nickel",
-                           10 => "dime",
-                           25 => "quarter"}
+  coin_types_singlular = { 1 => " penny",
+                           5 => " nickel",
+                           10 => " dime",
+                           25 => " quarter"}
 
-  coin_types_plural = { 1 => "pennies",
-                        5 => "nickels",
-                        10 => "dimes",
-                        25 => "quarters" }
+  coin_types_plural = { 1 => " pennies",
+                        5 => " nickels",
+                        10 => " dimes",
+                        25 => " quarters" }
 
   define_method(:coin_combinations) do
-    return "3 pennies"
+
+
+    case self
+      when 1..4
+        #PENNIES
+        if self == 1
+          return self.to_s + coin_types_singlular.fetch( 1 )
+        else
+          return self.to_s + coin_types_plural.fetch( 1 )
+        end
+      when 5..9
+        #NICKLES
+      when 10..24
+        #DIMES
+      when 25..99
+        #QUATERS
+    end
   end
 end
--- spec/coin_combinations_spec.rb
+++ spec/coin_combinations_spec.rb
@@ -2,6 +2,9 @@ require('rspec')
 require('coin_combinations')
 
 describe('Fixnum#coin_combinations') do
+  it('gives single penny when change is 1 cent') do
+    expect(1.coin_combinations).to( eq("1 penny"))
+  end
   it('gives pennies when change is less than 5 cents') do
     expect(3.coin_combinations).to( eq("3 pennies"))
   end
