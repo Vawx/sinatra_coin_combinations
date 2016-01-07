a4bffbaf480e47d0e77e433e41ad4061ef8b824d
Greg Kanteres and Kyle Langley <altck8@gmail.com>
2016-01-07 08:27:53 -0800

    inlude rspec testing for pennies

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -1,5 +1,15 @@
 class Fixnum
-  define_method(:coin_combinations) do
+  coin_types_singlular = { 1 => "penny",
+                           5 => "nickel",
+                           10 => "dime",
+                           25 => "quarter"}
 
+  coin_types_plural = { 1 => "pennies",
+                        5 => "nickels",
+                        10 => "dimes",
+                        25 => "quarters" }
+
+  define_method(:coin_combinations) do
+    return "3 pennies"
   end
 end
--- spec/coin_combinations_spec.rb
+++ spec/coin_combinations_spec.rb
@@ -1,2 +1,8 @@
 require('rspec')
 require('coin_combinations')
+
+describe('Fixnum#coin_combinations') do
+  it('gives pennies when change is less than 5 cents') do
+    expect(3.coin_combinations).to( eq("3 pennies"))
+  end
+end
